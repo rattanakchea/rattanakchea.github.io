bd41a89e4882ea9e46b2a3857477c7b915c59c3a
Rattanak Chea <rattanak.chea@dpworld.com>
2020-03-09 16:54:55 -0700

    clean up, seperate project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroductionComponent
+    IntroductionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
