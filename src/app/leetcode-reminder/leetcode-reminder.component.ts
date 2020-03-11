f4041a0418d9f1b6671c32749c2a543d8637710b
Rattanak Chea <rattanak.chea@dpworld.com>
2020-03-10 23:31:17 -0700

    add test http call

--- src/app/leetcode-reminder/leetcode-reminder.component.ts
+++ src/app/leetcode-reminder/leetcode-reminder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { delay } from "rxjs/operators";
 
 @Component({
   selector: "app-leetcode-reminder",
@@ -8,11 +10,17 @@ import { Component, OnInit } from "@angular/core";
 export class LeetcodeReminderComponent implements OnInit {
   email: string = "rattanak22@gmail.com";
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  projectUrl = "assets/json/projects.json";
 
   ngOnInit(): void {}
 
   onSubmit() {
     console.log("You submitted: ", this.email);
+
+    // Todo replace with my end-point to accept email, timestamp
+    // Test loading spinner with interceptor
+    return this.http.get(this.projectUrl).pipe(delay(5000));
   }
 }
