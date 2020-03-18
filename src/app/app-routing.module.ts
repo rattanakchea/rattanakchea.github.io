e48870a7daa1e5042b79543e14f2014c0f166b21
Rattanak Chea <rattanak.chea@dpworld.com>
2020-03-18 13:41:36 -0700

    must use lazy load for child router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,57 +1,62 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
 import { ProjectsComponent } from "./projects/projects.component";
-import { LeetcodeReminderComponent } from "./leetcode-reminder/leetcode-reminder.component";
 import { Page404Component } from "./shared/page404/page404.component";
+import { HomeComponent } from "./home/home.component";
 import { FullscreenComponent } from "./layouts/fullscreen/fullscreen.component";
+import { LeetcodeReminderComponent } from "./leetcode-reminder/leetcode-reminder.component";
 import { DefaultComponent } from "./layouts/default/default.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: DefaultComponent,
-    children: [
-      {
-        path: "",
-        component: DefaultComponent
-      },
-      {
-        path: "projects",
-        component: ProjectsComponent
-      }
-    ]
-  },
-  {
-    path: "projects",
-    component: ProjectsComponent
-  },
-  {
-    path: "leetcode-reminder",
-    component: LeetcodeReminderComponent
-  },
-  {
-    path: "song-lyrics",
     loadChildren: () =>
-      import("./song-lyrics/song-lyrics.module").then(m => m.SongLyricsModule)
+      import("./layouts/default/default.module").then(m => m.DefaultModule)
   },
-  {
-    path: "algorithm-visualizer",
-    loadChildren: () =>
-      import("./algorithm-visualizer/algorithm-visualizer.module").then(
-        m => m.AlgorithmVisualizerModule
-      )
-  },
-  {
-    path: "",
-    component: FullscreenComponent,
-    children: [
-      {
-        path: "covid19-dashboard",
-        component: FullscreenComponent
-      }
-    ]
-  },
+  // {
+  //   path: "",
+  //   component: DefaultComponent,
+  //   children: [
+  //     {
+  //       path: "",
+  //       component: DefaultComponent
+  //     },
+  //     {
+  //       path: "projects",
+  //       component: ProjectsComponent
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: "projects",
+  //   component: ProjectsComponent
+  // },
+  // {
+  //   path: "leetcode-reminder",
+  //   component: LeetcodeReminderComponent
+  // },
+  // {
+  //   path: "song-lyrics",
+  //   loadChildren: () =>
+  //     import("./song-lyrics/song-lyrics.module").then(m => m.SongLyricsModule)
+  // },
+  // {
+  //   path: "algorithm-visualizer",
+  //   loadChildren: () =>
+  //     import("./algorithm-visualizer/algorithm-visualizer.module").then(
+  //       m => m.AlgorithmVisualizerModule
+  //     )
+  // },
+  // {
+  //   path: "",
+  //   component: FullscreenComponent,
+  //   children: [
+  //     {
+  //       path: "covid19-dashboard",
+  //       component: FullscreenComponent
+  //     }
+  //   ]
+  // },
   {
     path: "**",
     component: Page404Component
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -2,11 +2,30 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DefaultComponent } from "./default.component";
 import { SharedModule } from "src/app/shared/shared.module";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
+import { ProjectsComponent } from "src/app/projects/projects.component";
+import { HomeComponent } from "src/app/home/home.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "projects",
+        component: ProjectsComponent
+      }
+    ]
+  }
+];
 
 // default layout for Portfolio site
 @NgModule({
   declarations: [DefaultComponent],
-  imports: [CommonModule, RouterModule, SharedModule]
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule]
 })
 export class DefaultModule {}
