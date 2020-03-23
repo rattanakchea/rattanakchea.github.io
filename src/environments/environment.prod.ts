fca306bf7666a430a7d1f1fca862002d6c405956
Rattanak Chea <rattanak.chea@dpworld.com>
2020-03-22 23:48:34 -0700

    npm run deploy:gh-page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 import { commonEnvironment } from "./environment.common";
 
+const PORT = 3000;
+
 export const environment = {
   production: true,
+  rootUrl: `http://localhost:${PORT}/`, //TODO replace with my prod API
   ...commonEnvironment
 };
