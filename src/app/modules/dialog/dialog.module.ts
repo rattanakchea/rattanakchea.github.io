24c1deea7456d1e18e4e6a48c7e129828f886b49
Rattanak Chea <rattanak.chea@dpworld.com>
2020-03-29 20:38:35 -0700

    set up dialog module and service

---
+++ src/app/modules/dialog/dialog.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+// dialog module that is reuseable
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule]
+})
+export class DialogModule {}
