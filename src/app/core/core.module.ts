8d4037b20b37cc3f7f091140ddd57c02ed1d1ffd
Rattanak Chea <rattanak.chea@dpworld.com>
2021-05-05 19:13:20 -0700

    introduction component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { RouterModule } from '@angular/router';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 // import { Page404Component } from "./page404/page404.component";
 // import { LoadingScreenComponent } from "./loading-screen/loading-screen.component";
 // import { DynamicNavComponent } from "./dynamic-nav/dynamic-nav.component";
-// import { IntroductionComponent } from "./introduction/introduction.component";
 
-const commonComponents = [NavigationComponent];
+const commonComponents = [NavigationComponent, IntroductionComponent];
 @NgModule({
   declarations: [...commonComponents],
   imports: [CommonModule, RouterModule],
