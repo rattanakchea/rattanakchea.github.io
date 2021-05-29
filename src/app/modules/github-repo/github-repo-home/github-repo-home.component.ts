b108137aba75e0efebe7f270475cf284f723a478
Rattanak Chea <rattanak.chea@dpworld.com>
2021-05-28 22:01:26 -0700

    base actionState facade

--- src/app/modules/github-repo/github-repo-home/github-repo-home.component.ts
+++ src/app/modules/github-repo/github-repo-home/github-repo-home.component.ts
@@ -1,15 +1,34 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IActionState, GithubFacadeService } from '../github-facade.service';
 
 @Component({
   selector: 'app-github-repo-home',
   templateUrl: './github-repo-home.component.html',
-  styleUrls: ['./github-repo-home.component.scss']
+  styleUrls: ['./github-repo-home.component.scss'],
 })
 export class GithubRepoHomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  sub: Subscription | null = null;
+  actionState: IActionState;
+  constructor(private GithubFacadeService: GithubFacadeService) {
+    this.actionState = this.GithubFacadeService.actionState;
   }
 
+  ngOnInit(): void {
+    // subscription to
+
+    this.GithubFacadeService.actionState$.subscribe((state) => {
+      console.log('subscribed state: ', state);
+
+      // with the state provided, call Github API
+    });
+  }
+
+  // can be removed, by using GithubFacadeService.setState(state) directly in the template
+  onSubmit(state: IActionState) {
+    console.log('state: ', state);
+
+    this.GithubFacadeService.setState(state);
+  }
 }
