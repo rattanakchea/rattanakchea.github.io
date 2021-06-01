e7954a85dc078499d54c1d506e11b9374b9015d4
Rattanak Chea <rattanak.chea@dpworld.com>
2021-06-01 16:58:15 -0700

    operators, ellipsis

---
+++ src/app/shared/operators.ts
@@ -0,0 +1,23 @@
+import { Observable, Subject, defer } from 'rxjs';
+import { finalize, map } from 'rxjs/operators';
+
+export const prepare = <T>(callback: () => void) => {
+  return (source: Observable<T>): Observable<T> =>
+    defer(() => {
+      callback();
+      return source;
+    });
+};
+
+export const indicate = <T>(indicator: Subject<boolean>) => {
+  return (source: Observable<T>): Observable<T> =>
+    source.pipe(
+      prepare(() => indicator.next(true)),
+      finalize(() => indicator.next(false))
+    );
+};
+
+export const toClass =
+  <T>(ClassType: { new (): T }) =>
+  (source: Observable<T>) =>
+    source.pipe(map((val) => Object.assign(new ClassType(), val)));
